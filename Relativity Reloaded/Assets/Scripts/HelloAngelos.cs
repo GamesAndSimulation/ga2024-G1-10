e5f21e9a727fac38e1f7928ca1ad1a6844215350
Lorenzo Massone <lorenzomassone@gmail.com>
2024-05-01 14:15:36 +0100

    Added print for today date

---
+++ Relativity Reloaded/Assets/Scripts/HelloAngelos.cs
@@ -0,0 +1,10 @@
+using System;
+
+class HelloAngelos
+{
+    static void Main()
+    {
+        DateTime today = DateTime.Now;
+        Console.WriteLine("Today's date is: " + today.ToString("yyyy-MM-dd"));
+    }
+}
