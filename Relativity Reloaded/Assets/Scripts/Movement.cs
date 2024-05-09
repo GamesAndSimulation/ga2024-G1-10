864df0a2998142891d252dddb6c813bceb364d56
Lorenzo Massone <lorenzomassone@gmail.com>
2024-05-09 19:12:41 +0100

    feat: Add pavement textures pack

--- Relativity Reloaded/Assets/Scripts/Movement.cs
+++ Relativity Reloaded/Assets/Scripts/Movement.cs
@@ -3,14 +3,38 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed = 10.0f;
+    public float jumpForce = 2.0f;
     private Vector3 _moveDirection = Vector3.zero;
     private Rigidbody _rb;
     private Animator _animator;
     private Transform _physicalBody;
     private Transform _mainCamera;
+    private bool isJumping = false;
     private static readonly int Speed = Animator.StringToHash("Speed");
 
     void Start()
+    {
+        InitializeComponents();
+    }
+
+    void Update()
+    {
+        HandleMovementInput();
+        HandleJumpInput();
+        UpdateAnimatorParameters();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        HandleCollision(collision);
+    }
+
+    void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+    private void InitializeComponents()
     {
         _rb = GetComponent<Rigidbody>();
         _physicalBody = transform.Find("PhysicalBody");
@@ -23,7 +47,7 @@ public class Movement : MonoBehaviour
         }
     }
 
-    void Update()
+    private void HandleMovementInput()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
@@ -39,12 +63,31 @@ public class Movement : MonoBehaviour
 
         _moveDirection = forward * verticalInput + right * horizontalInput;
         _moveDirection *= speed;
+    }
 
-        // Set the "speed" parameter of the Animator based on the magnitude of the movement direction
+    private void HandleJumpInput()
+    {
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            _rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+            isJumping = true;
+        }
+    }
+
+    private void UpdateAnimatorParameters()
+    {
         _animator.SetFloat(Speed, _moveDirection.magnitude);
     }
 
-    void FixedUpdate()
+    private void HandleCollision(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void MoveCharacter()
     {
         if (_moveDirection != Vector3.zero)
         {
