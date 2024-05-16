b57f2178ea0ea047f68f563bb890de348ecdd320
Lorenzo Massone <lorenzomassone@gmail.com>
2024-05-16 17:17:23 +0100

    added a pursuing enemy
    animations need to be fixed

--- Relativity Reloaded/Assets/Project/Internal/Scripts/Bullet.cs
+++ Relativity Reloaded/Assets/Project/Internal/Scripts/Bullet.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
 
+using UnityEngine;
+
 public class Bullet : MonoBehaviour
 {
+    public float lifetime = 5f; // Lifetime of the bullet in seconds
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         // Handle bullet collision here
