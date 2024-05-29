945d55a5d3144ece5c4b997696b9f298b367bd6c
YungSkang <100635152+YungSkang@users.noreply.github.com>
2024-05-29 02:38:00 +0100

    Menu and Intro Scene Added
    Sound Effects and Enemy (Maybe also boss fight) needed

---
+++ Relativity Reloaded/Assets/Scenes/LoadAfterIntro.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadAfterIntro : MonoBehaviour
+{
+    [SerializeField] private float delayBeforeLoading = 90f;
+    private float timeElapsed;
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed > delayBeforeLoading) {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
