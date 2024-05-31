87c7543233cad08757677bb535970722083794c6
Lorenzo Massone <lorenzomassone@gmail.com>
2024-05-31 15:21:32 +0100

    chore: Set initial state of DimensionalObject in Start method

--- Relativity Reloaded/Assets/Scripts/DimensionalObject.cs
+++ Relativity Reloaded/Assets/Scripts/DimensionalObject.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class DimensionalObject : MonoBehaviour
 {
+    public bool initialState = true; // Set this in the inspector to define the initial state
+
     private void Start()
     {
         DimensionalObjectManager.Instance.RegisterDimensionalObject(gameObject);
+        SetInitialState();
     }
 
     private void OnDestroy()
@@ -13,5 +16,10 @@ public class DimensionalObject : MonoBehaviour
         {
             DimensionalObjectManager.Instance.UnregisterDimensionalObject(gameObject);
         }
+    }
+
+    private void SetInitialState()
+    {
+        gameObject.SetActive(initialState);
     }
 }
